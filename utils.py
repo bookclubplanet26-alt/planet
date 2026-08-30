d2a461919e95da85e14f124acc1567896731ffc9
hanjisubusiness22222 <hanjisubusiness22@gmail.com>
2026-08-30 18:47:14 +0900

    fix: update webhook URL for author and rating parameters

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ GOOGLE_SHEET_ID = "1UbvS5tDzQvGlOh-TVagtYJ31pW9u8CNw-wENIK8iK48"
 
 # 사용자가 출석 기록용으로 제공한 구글 시트 ID 및 Webhook URL
 GOOGLE_SHEET_ATTENDANCE_ID = "1k1lJmH6fmsPKD8h_-QMbTVy6nrh-RTJt-fUJAQWukKE"
-ATTENDANCE_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbwipigj0WIpvBO1OcBmSj7yTqlnuleRS3E3RkxW1OIrw7eP3ZEDKuEYD4GKIOGsxxggVA/exec"
+ATTENDANCE_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzL2qeg6F63u2FGZvxd_vLb_f0D_VYYKPwjPyxD2hhP7a0ifYcMfK9oOBudkXAp5fbu/exec"
 
 def get_club_season_code(dt=None):
     """
